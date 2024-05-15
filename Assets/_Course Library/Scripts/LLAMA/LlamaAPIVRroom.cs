1984ed452cdb7eb2780d8c681152ef5c99c5b172
Pranjali Barve <barvep@oregonstate.edu>
2024-05-15 13:48:00 -0700

    LLM response on UI enabled

--- Assets/_Course Library/Scripts/LLAMA/LlamaAPIVRroom.cs
+++ Assets/_Course Library/Scripts/LLAMA/LlamaAPIVRroom.cs
@@ -38,15 +38,6 @@ public static class LlamaAPIVRroom
             writer.Write(jsonPayload);
         }
 
-        ////Non streaming
-        ////send request and get response
-        //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-        ////read JSON response body
-        //StreamReader reader = new StreamReader(response.GetResponseStream());
-        //string json = reader.ReadToEnd();
-        ////Deserialize JSON to LlamaPromptResponse object type
-        //return JsonUtility.FromJson<LlamaPromptResponse>(json);
-
 
         using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
         {
@@ -77,6 +68,43 @@ public static class LlamaAPIVRroom
 
             }
         }
+
+    }
+
+    public static LlamaPromptResponse GetPromptResponseNonStreaming(string model, string prompt, int maxTokens, bool stream)
+    {
+        //Constructing JSON payload
+        ApiJsonPayload requestPayload = new ApiJsonPayload
+        {
+            model = model,
+            prompt = prompt,
+            max_tokens = maxTokens,
+            stream = stream
+        };
+
+        //Serialize the payload to JSON
+        string jsonPayload = JsonUtility.ToJson(requestPayload);
+
+        //Creat HTTP POST request 
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:1234/v1/completions");
+
+        request.Method = "POST";
+        request.ContentType = "application/json";
+
+        // Write the JSON payload to the request body
+        using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+        {
+            writer.Write(jsonPayload);
+        }
+
+        //Non streaming
+        //send request and get response
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //read JSON response body
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string json = reader.ReadToEnd();
+        //Deserialize JSON to LlamaPromptResponse object type
+        return JsonUtility.FromJson<LlamaPromptResponse>(json);
 
     }
 
--- Assets/_Course Library/Scripts/LLAMA/UILLAMA.cs
+++ Assets/_Course Library/Scripts/LLAMA/UILLAMA.cs
@@ -19,40 +19,87 @@ public class UILLAMA : MonoBehaviour
     //}
 
     public TextMeshProUGUI output;
-   
+    public string outputHolder;
 
-    public void OnTriggerStay(Collider other)
+
+    public void OkayButton()
     {
-        
+        output.text = "";
+        StartCoroutine(GenerateStreamingResponseButton());
+    }
 
+    public void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.name == "RightHand Controller")
         {
-            string model = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q3_K_L.gguf";
-            string prompt = "what is a " + this.gameObject.name;
-            int maxTokens = -1;
-            bool stream = true;
 
-            StartCoroutine(StreamPromptResponse(model, prompt, maxTokens, stream));
+            //GenerateNonStreamingResponse();
+            StartCoroutine(GenerateStreamingResponse());
+
+            ////Simple Object picking test
             //output.text = "this is a " + this.gameObject.name;
         }
-        
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        output.text = outputHolder;     
     }
     public void OnTriggerExit(Collider other)
     {
         output.text = "";
     }
 
-    IEnumerator StreamPromptResponse(string model, string prompt, int maxTokens, bool stream)
+    IEnumerator GenerateStreamingResponse()
     {
-        LlamaAPI.GetPromptResponse(model, prompt, maxTokens, stream, (completionResponse) =>
+        string model = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q3_K_L.gguf";
+        string prompt = "what is a " + this.gameObject.name;
+        int maxTokens = -1;
+        bool stream = true;
+
+        LlamaAPIVRroom.GetPromptResponse(model, prompt, maxTokens, stream, (completionResponse) =>
         {
             // Loop through the choices and update output.text
             foreach (var choice in completionResponse.choices)
             {
-                output.text += choice.text;
+               outputHolder += choice.text;
+               
             }
         });
         yield return null;
     }
 
+    IEnumerator GenerateStreamingResponseButton()
+    {
+        string model = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q3_K_L.gguf";
+        string prompt = "what is a tree?";
+        int maxTokens = -1;
+        bool stream = true;
+
+        LlamaAPIVRroom.GetPromptResponse(model, prompt, maxTokens, stream, (completionResponse) =>
+        {
+            // Loop through the choices and update output.text
+            foreach (var choice in completionResponse.choices)
+            {
+                output.text += choice.text;
+
+            }
+        });
+        yield return null;
+    }
+
+    private void GenerateNonStreamingResponse()
+    {
+        //Non streaming
+        string model = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF/mistral-7b-instruct-v0.1.Q3_K_L.gguf";
+        
+        string prompt = "what is a " + this.gameObject.name;
+
+        int maxTokens = -1;
+        bool stream = false;
+
+        LlamaPromptResponse promptOutput = LlamaAPIVRroom.GetPromptResponseNonStreaming(model, prompt, maxTokens, stream);
+        //output.text = promptOutput.choices[0].text;
+        outputHolder = promptOutput.choices[0].text;
+       
+    }
 }
